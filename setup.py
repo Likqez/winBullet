9567fe5009bb15c386fa4afae85aa835a66b7949
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-04-17 09:13:20 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='winBullet',
-      version='1.0',
+      version='1.1',
       description='Beautiful Python prompts made simple.',
       long_description="Extensive support for Python list prompts \
             formatting and colors",
